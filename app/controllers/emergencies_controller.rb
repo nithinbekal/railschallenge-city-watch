53edc3c394759adb93fcc025e00740aea2e31da2
Nithin Bekal <nithinbekal@gmail.com>
2016-05-03 15:45:24 +0530

    emergencies#update

--- app/controllers/emergencies_controller.rb
+++ app/controllers/emergencies_controller.rb
@@ -1,4 +1,8 @@
 class EmergenciesController < ApplicationController
+  def show
+    @emergency = Emergency.find_by!(code: params[:id])
+  end
+
   def create
     @emergency = Emergency.new(create_params)
     if @emergency.save
@@ -8,9 +12,22 @@ class EmergenciesController < ApplicationController
     end
   end
 
+  def update
+    @emergency = Emergency.find_by!(code: params[:id])
+    if @emergency.update(update_params)
+      render :show, status: :ok
+    else
+      render json: { message: @emergency.errors }
+    end
+  end
+
   private
 
   def create_params
     params.require(:emergency).permit(:code, :fire_severity, :medical_severity, :police_severity)
+  end
+
+  def update_params
+    params.require(:emergency).permit(:fire_severity, :medical_severity, :police_severity, :resolved_at)
   end
 end
--- app/views/emergencies/show.jbuilder
+++ app/views/emergencies/show.jbuilder
@@ -1,3 +1,3 @@
 json.emergency do
-  json.extract! @emergency, :code, :fire_severity, :police_severity, :medical_severity
+  json.extract! @emergency, :code, :fire_severity, :police_severity, :medical_severity, :resolved_at
 end
